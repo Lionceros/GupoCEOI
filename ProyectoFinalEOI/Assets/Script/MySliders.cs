d7701663f6471cb001dc2ecdd91e13d7e687491a
Groddar <84792447+Groddar@users.noreply.github.com>
2021-06-03 16:11:49 +0200

    Actualizando
    Lunes 01/06/2021

---
+++ ProyectoFinalEOI/Assets/Script/MySliders.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
+[RequireComponent(typeof(Slider))]
+
+public class MySliders : MonoBehaviour
+{
+    Slider slider
+    {
+        get { return GetComponent<Slider>(); }
+    }
+    public AudioMixer mixer;
+
+    [SerializeField]
+    private string volumeName;
+
+    [SerializeField]
+    private Text volumeLabel;
+
+    private void Start()
+    {
+        UpdateValueOnChange(slider.value);
+
+        slider.onValueChanged.AddListener(delegate
+        {
+            UpdateValueOnChange(slider.value);
+        });
+    }
+    public void UpdateValueOnChange(float value)
+    {
+        if(mixer != null)
+            mixer.SetFloat(volumeName, Mathf.Log(value) * 20f);
+
+        if (volumeLabel != null)
+            volumeLabel.text = Mathf.Round(value * 100.0f).ToString() + "%";
+    }
+}

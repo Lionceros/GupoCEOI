8f08642228a9238d85e965d398b38728d777a867
Lionceros <55964492+Lionceros@users.noreply.github.com>
2021-06-03 14:47:56 +0200

    03_06_2021
    Juntado la pantalla inicio y añadido el panel de derrota
    Todas las animaciones añadidas

--- ProyectoFinalEOI/Assets/Script/UiManager.cs
+++ ProyectoFinalEOI/Assets/Script/UiManager.cs
@@ -11,13 +11,14 @@ public class UiManager : MonoBehaviour
     public GameObject[] prefabsEnemy;
     public GameObject invokeEnemy;
 
+    [SerializeField]
+    //UnitCharacter unit;
+
     public float timeSeconds = 0f;
     public float timeActual = 0f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -28,34 +29,48 @@ public class UiManager : MonoBehaviour
 
     public void InvokeEnemy()
     {
-        int tempRandom = RandomNumber(0, 2);
+        int tempRandom = RandomNumber(0, 4);
         Instantiate(prefabsEnemy[tempRandom], invokeEnemy.transform);
-
+        //unit.SortingOrder();
     }
 
 
     public void InvokeAllyWarrior()
     {
         Instantiate(prefabsAlly[0], invokeAlly.transform);
+        //unit.SortingOrder();
     }
     public void InvokeAllyArcher()
     {
         Instantiate(prefabsAlly[1], invokeAlly.transform);
+        //unit.SortingOrder();
     }
 
-    public int RandomNumber(int min, int max)
+    public void InvokeAllyRogue()
     {
-        return Random.Range(min, max);
+        Instantiate(prefabsAlly[2], invokeAlly.transform);
+        //unit.SortingOrder();
+    }
+
+    public void InvokeAllyMage()
+    {
+        Instantiate(prefabsAlly[3], invokeAlly.transform);
+        //unit.SortingOrder();
     }
 
     public void TimeToInvokeEnemy()
     {
         timeActual += Time.fixedDeltaTime;
-        if (timeActual - timeSeconds >= 20f)
+        if (timeActual - timeSeconds >= 5f)
         {
             timeSeconds = timeActual;
 
             InvokeEnemy();
         }
+    }
+
+    public int RandomNumber(int min, int max)
+    {
+        return Random.Range(min, max);
     }
 }
